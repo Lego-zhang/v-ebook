b23d5c8212b6670a436a96c7860e0ec2a63ed3b7
Lego-zhang <1945465017@qq.com>
2019-05-24 16:45:21 +0800

    准备好所用到的资源

--- README.md
+++ README.md
@@ -27,3 +27,6 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### 下载的依赖包
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span class="icon-bookmark "></span>
+    <span class="text">123123123</span>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
+<style>
+  .text {
+    font-family: "Days One";
+    font-size: 20px;
+    color:orange;
+  }
+</style>
